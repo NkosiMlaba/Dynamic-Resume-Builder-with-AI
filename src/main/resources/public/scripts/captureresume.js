d92d35d96fd8618b23faae500534ab2660605ca3
Nkosikhona Mlaba <nmlaba023@student.wethinkcode.co.za>
2024-09-17 12:59:59 +0200

    html and javascript updates

---
+++ src/main/resources/public/scripts/captureresume.js
@@ -0,0 +1,11 @@
+
+function captureResume() {
+    console.log('capturing resume');
+}
+
+// Main function that calls the sub-functions
+function main() {
+}
+
+// Call main function when the page fully loads
+window.onload = main;
