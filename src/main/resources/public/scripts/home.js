ea79e33191da4b3c507b2d38dfdc1a20bba53fa8
Nkosikhona Mlaba <nmlaba023@student.wethinkcode.co.za>
2024-09-17 12:00:02 +0200

    added button navigation for home page

---
+++ src/main/resources/public/scripts/home.js
@@ -0,0 +1,17 @@
+
+
+function loadLoginPage() {
+    window.location.href = "login.html";
+}
+
+function loadCreateUserPage() {
+    window.location.href = "createuser.html";
+}
+
+
+// Main function that calls the sub-functions
+function main() {
+}
+
+// Call main function when the page fully loads
+window.onload = main;
