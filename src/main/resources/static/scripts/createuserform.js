ab4fb45466bf6d2aa5a179283ed554ea80ce8015
Nkosikhona Mlaba <nkosimlaba397@gmail.com>
2024-09-19 04:27:57 +0200

    updated asset reference in javascript code

--- src/main/resources/static/scripts/createuserform.js
+++ src/main/resources/static/scripts/createuserform.js
@@ -3,20 +3,20 @@ document.getElementById('login').addEventListener('submit', async function(event
     
     const form = event.target;
     const formData = new FormData(form);
-  
+    
     try {
-      const response = await fetch('http://localhost:7000/login', {
+        const response = await fetch('http://localhost:7000/login', {
         method: 'POST',
         body: formData
-      });
-  
-      if (response.ok) {
+        });
+    
+        if (response.ok) {
         const result = await response.json();
         console.log('Form submitted successfully:', result);
-      } else {
+        } else {
         console.error('Error submitting form:', response.status, response.statusText);
-      }
+        }
     } catch (error) {
-      console.error('Error submitting form:', error);
+        console.error('Error submitting form:', error);
     }
-  });
+    });
--- src/main/resources/static/scripts/formsubmission.js
+++ src/main/resources/static/scripts/formsubmission.js
@@ -3,20 +3,20 @@ document.getElementById('create').addEventListener('submit', async function(even
     
     const form = event.target;
     const formData = new FormData(form);
-  
+    
     try {
-      const response = await fetch('http://localhost:7000/register', {
+        const response = await fetch('http://localhost:7000/register', {
         method: 'POST',
         body: formData
-      });
-  
-      if (response.ok) {
+        });
+    
+        if (response.ok) {
         const result = await response.json();
         console.log('Form submitted successfully:', result);
-      } else {
+        } else {
         console.error('Error submitting form:', response.status, response.statusText);
-      }
+        }
     } catch (error) {
-      console.error('Error submitting form:', error);
+        console.error('Error submitting form:', error);
     }
-  });
+    });
